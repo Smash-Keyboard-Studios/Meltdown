0295f00b2bc0efd524d6c44080d4a486dcd93738
odin <odin.dioszegi@gmail.com>
2024-02-05 19:19:15 +0000

    added ui elements
    added buttons and text with some script tests

---
+++ StudioUnityProject/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [HideInInspector]
+    public float somethingwhatever;
+    [SerializeField]
+    private bool coolbeans23;
+
+    public void Dothis()
+    {
+        print("helo :)");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
