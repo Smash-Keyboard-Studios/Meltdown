bbdaa824fdd2d7654b844450d0cfeaf89c4b6a06
shanewdhall <shanewdhall@gmail.com>
2024-02-08 16:41:28 +0000

    experimented with making the gun on a new scene

---
+++ StudioUnityProject/Assets/Gun.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefableftclick;
+    public GameObject bulletPrefabrightclick;
+    public float bulletSpeed = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Instantiate the bullet GameObject
+            GameObject bulletObject = Instantiate(bulletPrefableftclick, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+            // Check if the bullet GameObject is not null
+            if (bulletObject != null)
+            {
+                // Get the Rigidbody component of the bullet GameObject
+                Rigidbody bulletRigidbody = bulletObject.GetComponent<Rigidbody>();
+
+                // Check if the bullet Rigidbody component is not null
+                if (bulletRigidbody != null)
+                {
+                    // Apply velocity to the bullet Rigidbody
+                    bulletRigidbody.velocity = bulletSpawnPoint.forward * bulletSpeed;
+                }
+               
+            }
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            // Instantiate the bullet GameObject
+            GameObject bulletObject = Instantiate(bulletPrefabrightclick, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+            // Check if the bullet GameObject is not null
+            if (bulletObject != null)
+            {
+                // Get the Rigidbody component of the bullet GameObject
+                Rigidbody bulletRigidbody = bulletObject.GetComponent<Rigidbody>();
+
+                // Check if the bullet Rigidbody component is not null
+                if (bulletRigidbody != null)
+                {
+                    // Apply velocity to the bullet Rigidbody
+                    bulletRigidbody.velocity = bulletSpawnPoint.forward * bulletSpeed;
+                }
+
+            }
+
+        }
+    }
+}
