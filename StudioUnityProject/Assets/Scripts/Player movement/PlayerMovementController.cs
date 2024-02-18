67cca0b889d313b6dfe87b06c286d57ecaa5e073
DM <dommcneill@outlook.com>
2024-02-18 22:21:20 +0000

    Player has physics and crouching has detection

--- StudioUnityProject/Assets/Scripts/Player movement/PlayerMovementController.cs
+++ StudioUnityProject/Assets/Scripts/Player movement/PlayerMovementController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
@@ -8,25 +9,58 @@ public class PlayerMovementController : MonoBehaviour
 	public bool Locked = false;
 
 
-	public float Speed = 5f;
+	public float WalkSpeed = 5f;
 	public float SprintSpeed = 10f;
 
 	public float Gravity = -9.81f;
 
 	public float JumpHeight = 1.4f;
 
+	public float AirMovementMultiplyer = 0.2f;
+
+	public float GroundResistance = 0.2f;
+	public float AirResistance = 0f;
+
 
 	private CharacterController _characterContoller;
 
-	private Vector3 velocity;
+	private Vector3 _velocity;
 
-	private bool isGrounded = false;
+	private float _speed = 0;
+
+	// 1 = normal speed. Its like speed penalty.
+	private float _crouchSpeedScale = 1;
+
+	private bool _isGrounded = false;
+
+	private bool _isUnableToUncrouch = false;
+
+	private bool _isCrouched = false;
+
+	private float _playerStandingHeight = 1.75f;
+	private float _playerCrouchingHeight;
+
+	private Transform _cameraHolderTransform;
+
+	private float _cameraStandingHeight = 0.6f;
+	private float _cameraCrouchHeight;
+
+	private bool _isSprinting = false;
+
+	private float _resistance = 0;
+
+	private bool _isOnSlope = false;
+	RaycastHit slopeHit;
 
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		_characterContoller = GetComponent<CharacterController>();
+		_cameraHolderTransform = Camera.main.transform.parent.GetComponent<Transform>();
+
+		_playerCrouchingHeight = _playerStandingHeight / 2f;
+		_cameraCrouchHeight = _cameraStandingHeight / 2f;
 
 		Locked = false;
 	}
@@ -37,74 +71,199 @@ public class PlayerMovementController : MonoBehaviour
 		HandleGravity();
 
 		if (Locked) return; // Stops movement
-		HandleMovement();
-		HandleJumping();
 		HandleGroundCheck();
 
+		_isOnSlope = IsOnSlope();
+
+
+		HandleMovement();
+		HandleJumping();
+
+		HandleCrouching();
+
+
+		HandleResistance();
+
+		_characterContoller.Move(_velocity * Time.deltaTime);
 
 	}
 
 	private void HandleMovement()
 	{
-		Vector3 moveDirection = Vector3.zero;
+		// redo with new input system.
+		float xAxis = Input.GetAxisRaw("Horizontal");
+		float zAxis = Input.GetAxisRaw("Vertical");
 
-		moveDirection.x = Input.GetAxisRaw("Horizontal");
-		moveDirection.z = Input.GetAxisRaw("Vertical");
-
-		moveDirection = transform.right * moveDirection.x + transform.forward * moveDirection.z;
+		Vector3 moveDirection = transform.right * xAxis + transform.forward * zAxis;
 
 		moveDirection.Normalize();
 
-		if (Input.GetKey(KeyCode.LeftShift))
+		if (Input.GetKey(KeyCode.LeftShift) && _isGrounded)
 		{
-			_characterContoller.Move(moveDirection * SprintSpeed * Time.deltaTime);
+			_isSprinting = true;
 		}
 		else
 		{
-			_characterContoller.Move(moveDirection * Speed * Time.deltaTime);
+			_isSprinting = false;
 		}
+
+
+		if (_isSprinting)
+		{
+			_speed = SprintSpeed;
+		}
+		else
+		{
+			_speed = WalkSpeed;
+		}
+
+		if (_isOnSlope)
+		{
+			moveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+		}
+
+		_velocity += moveDirection * (_isGrounded ? 1 : AirMovementMultiplyer) * _speed * _crouchSpeedScale;
+
+		float yVel = _velocity.y;
+		_velocity.y = 0;
+
+		if (_velocity.magnitude > _speed)
+		{
+			_velocity = _velocity.normalized * _speed;
+		}
+
+		_velocity.y = yVel;
+	}
+
+	private void HandleResistance()
+	{
+		// can reduce
+		if (_isGrounded && !_isOnSlope)
+		{
+			_resistance = GroundResistance;
+			_velocity += -_velocity * _resistance;
+		}
+		else if (!_isGrounded && !_isOnSlope)
+		{
+			_resistance = AirResistance;
+			_velocity += -_velocity * _resistance;
+		}
+		else if (_isOnSlope)
+		{
+			_resistance = GroundResistance;
+			// print(Vector3.ProjectOnPlane(_velocity, slopeHit.normal));
+			_velocity += -_velocity * _resistance;
+		}
+
+
 	}
 
 	private void HandleGravity()
 	{
-		if (isGrounded && velocity.y <= 0)
+		if (_isGrounded && !_isOnSlope && _velocity.y <= 0)
 		{
-			velocity.y = -2f;
+			_velocity.y = -2f;
 		}
-		else
+		else if (!_isOnSlope)
 		{
-			velocity.y += Gravity * Time.deltaTime;
+			_velocity.y += Gravity * Time.deltaTime;
 		}
 
-		_characterContoller.Move(velocity * Time.deltaTime);
+		_characterContoller.Move(_velocity * Time.deltaTime);
 
-		if (_characterContoller.velocity.y <= 0)
+		if (_characterContoller.velocity.y <= 0 && _velocity.y > _characterContoller.velocity.y)
 		{
 			// may change later
-			velocity.y = _characterContoller.velocity.y;
+			_velocity.y = _characterContoller.velocity.y;
 		}
 	}
 
 	private void HandleJumping()
 	{
-		if (Input.GetKey(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			if (isGrounded)
+			if (_isGrounded && !_isOnSlope)
 			{
-				velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+				_velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity * _resistance);
+			}
+			else if (_isOnSlope)
+			{
+				_velocity += (transform.up + slopeHit.normal) * Mathf.Sqrt(JumpHeight * -2f * Gravity * _resistance);
 			}
 		}
 	}
 
-	private void HandleGroundCheck()
+	private void HandleCrouching()
 	{
-		if (_characterContoller.isGrounded || Physics.Raycast(transform.position, -transform.up, 1.1f, ~(1 << 3)))
+
+		// player layer
+		int layer = 6;
+		int mask = ~(1 << layer);
+
+
+		if (_isCrouched && Physics.SphereCast(transform.position, _characterContoller.radius, transform.up, out RaycastHit hit, (_playerStandingHeight + _playerCrouchingHeight - _characterContoller.radius) * 0.5f, mask))
 		{
-			isGrounded = true;
+			_isUnableToUncrouch = true;
 		}
 		else
 		{
-			isGrounded = false;
+			_isUnableToUncrouch = false;
+		}
+
+		// TODO replace with new input system
+		if (_isUnableToUncrouch)
+		{
+			_characterContoller.height = _playerCrouchingHeight;
+			MoveCamY(_cameraCrouchHeight);
+
+		}
+
+		if (Input.GetKey(KeyCode.C))
+		{
+			_characterContoller.height = _playerCrouchingHeight;
+			MoveCamY(_cameraCrouchHeight);
+			_isCrouched = true;
+			_crouchSpeedScale = 0.5f;
+		}
+		else if (!_isUnableToUncrouch)
+		{
+			_characterContoller.height = _playerStandingHeight;
+			MoveCamY(_cameraStandingHeight);
+			_isCrouched = false;
+			_crouchSpeedScale = 1f;
+		}
+	}
+
+	private void MoveCamY(float y)
+	{
+		_cameraHolderTransform.localPosition = new Vector3(0, y, 0);
+	}
+
+	private bool IsOnSlope()
+	{
+		if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, _characterContoller.height)) // might need to make this value scale with player later
+		{
+			if (slopeHit.normal != Vector3.up)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+
+	private void HandleGroundCheck()
+	{
+		if (_characterContoller.isGrounded || Physics.Raycast(transform.position, -transform.up, (_characterContoller.height / 2f) + 0.1f))
+		{
+			_isGrounded = true;
+		}
+		else
+		{
+			_isGrounded = false;
 		}
 
 	}
