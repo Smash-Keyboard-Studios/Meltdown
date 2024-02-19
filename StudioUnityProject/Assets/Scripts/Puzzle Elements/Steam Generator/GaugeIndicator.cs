aa9cd1e15ee4b6553da4fc0c6eb88e71740f0dfc
Jack Ralls <jakilutra@gmail.com>
2024-02-19 20:03:55 +0000

    Added More Inspector Variables to Gauge Script
    Added Reset Pin Location (uses previously unused ResetGauge function)
    Added Final Pinlocation
    Added Instant Rotation
    Added Move To Prev Point
    
    I still can't get Prev Point + Instant Rotation to work and the Move to Prev Point uses the cool down speed (maybe there could be an option to switch between cool down speed and negative of the rotation speed and have the latter as the default).

--- StudioUnityProject/Assets/Scripts/Puzzle Elements/Steam Generator/GaugeIndicator.cs
+++ StudioUnityProject/Assets/Scripts/Puzzle Elements/Steam Generator/GaugeIndicator.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEditor;
 
 public class GaugeIndicator : MonoBehaviour
 {
@@ -11,9 +12,10 @@ public class GaugeIndicator : MonoBehaviour
 
 	private float[] _rotationPoint = { 0.0f, 35.0f, 70.0f, 105.0f, 140.0f, 175.0f };
 
-	private float _rotationIncrement, _nextRotationPoint;
-	private int _rotationIndex = 1;
-	private Quaternion _defaultRotation;
+ 	private float _firstRotationPoint, _nextRotationPoint, _prevRotationPoint, _finalRotationPoint;
+	private int _rotationIndex, _startRotationIndex, _finalRotationIndex;
+	private float _rotationIncrement;
+	private Quaternion _defaultRotation, _finalRotation;
 
 	// [Editor Variables]
 
@@ -22,9 +24,15 @@ public class GaugeIndicator : MonoBehaviour
 	[SerializeField][Range(0.0f, 100.0f)] private float _rotationSpeed = 40.0f;
 	[SerializeField][Range(0.0f, 100.0f)] private float _coolOffSpeed = 5.0f;
 	[SerializeField][Range(0.0f, 100.0f)] private float _coolOffDelay = 2.0f;
-
-	public bool MoveToNextPoint = false;
 	public bool DisableCoolOff = false;
+	public bool InstantRotation = false;
+
+	[Header("<b>Location Parameters</b>")]
+	[Space]
+	public bool MoveToNextPoint = false;
+	public bool MoveToPrevPoint = false;
+	public bool ResetPinLocation = false;
+	public bool FinalPinLocation = false;
 
 	public UnityEvent OnComplete;
 
@@ -32,18 +40,40 @@ public class GaugeIndicator : MonoBehaviour
 
 	private void Awake()
 	{
+		// Indexes of Array
+		_startRotationIndex = 1;
+		_finalRotationIndex = _rotationPoint.Length - 1;
+		_rotationIndex = _startRotationIndex;
+
+		// Elements of Array
 		_nextRotationPoint = _rotationPoint[_rotationIndex];
+		_finalRotationPoint = _rotationPoint[_finalRotationIndex];
+
+		// Rotations
 		_defaultRotation = transform.rotation;
+		_finalRotation = _defaultRotation * Quaternion.Euler(0, 0, -_finalRotationPoint);
 	}
 
 	private void Update()
 	{
-		if (MoveToNextPoint) // Determines if rotation is enabled.
+		if (ResetPinLocation) 
 		{
+			ResetPinLocation = false;
+			ResetGauge();
+		}
+		else if (FinalPinLocation)
+		{
+			FinalPinLocation = false;
+			MaxGauge();
+		}
+		else if (MoveToNextPoint) // Determines if forward rotation is enabled.
+		{
+			MoveToPrevPoint = false;
 			IncrementGauge();
 		}
-		else if (!DisableCoolOff && transform.rotation != _defaultRotation) // Cools down (reverse rotation) to origin if not.
+		else if ((!DisableCoolOff && transform.rotation != _defaultRotation) || MoveToPrevPoint) // Cools down (reverse rotation) to origin if not.
 		{
+			MoveToNextPoint = false;
 			DecrementGauge();
         }
 	}
@@ -52,14 +82,24 @@ public class GaugeIndicator : MonoBehaviour
 
 	public void IncrementGauge()
 	{
-		if (_rotationIndex < _rotationPoint.Length) // Determines if there is a valid point to rotate to.
+		if (_rotationIndex <= _finalRotationIndex) // Determines if there is a valid point to rotate to.
 		{
 			// Active rotation towards the next point.
 
 			if (_rotationIncrement < _nextRotationPoint)
 			{
-				transform.Rotate(-Vector3.forward, _rotationSpeed * Time.deltaTime);
-				_rotationIncrement += _rotationSpeed * Time.deltaTime;
+				if (!InstantRotation)
+				{
+					transform.Rotate(-Vector3.forward, _rotationSpeed * Time.deltaTime);
+					_rotationIncrement += _rotationSpeed * Time.deltaTime;
+				}
+				else 
+				{
+					transform.Rotate(-Vector3.forward, _nextRotationPoint - _rotationIncrement);
+					_rotationIncrement = _nextRotationPoint;
+					_rotationIndex++;
+					SetRotationPoints(_rotationIndex);
+				}
 			}
 
 			// When the point is reached, rotation is disabled, and the next point (to rotate to) is assigned.
@@ -68,10 +108,10 @@ public class GaugeIndicator : MonoBehaviour
 			{
 				MoveToNextPoint = false;
 
-                if (_rotationIndex < _rotationPoint.Length - 1) // Determines if the current point is not the last one.
+                if (_rotationIndex < _finalRotationIndex) // Determines if the current point is not the last one.
 				{
 					_rotationIndex++;
-					_nextRotationPoint = _rotationPoint[_rotationIndex];
+					SetRotationPoints(_rotationIndex);
                     DisableCoolOff = true;
 					Invoke("awakenCoolDown", _coolOffDelay);
                 }
@@ -87,21 +127,37 @@ public class GaugeIndicator : MonoBehaviour
 
 	public void DecrementGauge()
 	{
-		transform.Rotate(-Vector3.forward, -_coolOffSpeed * Time.deltaTime);
-		_rotationIncrement -= _coolOffSpeed * Time.deltaTime;
+		if (_rotationIncrement > _prevRotationPoint)
+		{
+			if (!InstantRotation)
+			{
+				transform.Rotate(-Vector3.forward, -_coolOffSpeed * Time.deltaTime);
+				_rotationIncrement -= _coolOffSpeed * Time.deltaTime;
+			}
+			else 
+			{
+				transform.Rotate(-Vector3.forward, _prevRotationPoint - _rotationIncrement);
+				_rotationIncrement = _prevRotationPoint;
+			}
+		}
 
 		// If the cooling takes it back below a previous rotation point, that will then become the next point to reach.
-
-		if (_rotationIndex > 1 && _rotationIncrement < _rotationPoint[_rotationIndex - 1])
+		
+		else if (_rotationIncrement <= _prevRotationPoint)
 		{
-			_rotationIndex--;
-			_nextRotationPoint = _rotationPoint[_rotationIndex];
+			MoveToPrevPoint = false;
+
+			if (_rotationIndex > _startRotationIndex && _rotationIncrement < _prevRotationPoint)
+			{
+				_rotationIndex--;
+				SetRotationPoints(_rotationIndex);
+			}
 		}
 	}
 
 	public void awakenCoolDown()
 	{
-		if (!MoveToNextPoint && _rotationIndex < _rotationPoint.Length - 1) // if rotation is disabled and isn't last point.
+		if (!MoveToNextPoint && _rotationIndex < _finalRotationIndex) // if rotation is disabled and isn't last point.
 		{
 			DisableCoolOff = false;
 		}
@@ -109,13 +165,34 @@ public class GaugeIndicator : MonoBehaviour
 
 	public void ResetGauge()
 	{
-		// Reset rotation
+		// Resets rotation.
 		transform.rotation = _defaultRotation;
 
-		// Reset gauge-related variables
-		_rotationIncrement = 0.0f;
-		_rotationIndex = 1;
-		_nextRotationPoint = _rotationPoint[_rotationIndex];
+		// Resets gauge-related variables
+		_rotationIncrement = _firstRotationPoint;
+		_rotationIndex = _startRotationIndex;
+		SetRotationPoints(_rotationIndex);
 		MoveToNextPoint = false;
+		MoveToPrevPoint = false;
+	}
+
+	public void MaxGauge()
+	{
+		// Completes rotation.
+		transform.rotation = _finalRotation;
+
+		// Sets gauge-related variables to final destinations.
+		_rotationIncrement = _finalRotationPoint;
+		_rotationIndex = _finalRotationIndex;
+		SetRotationPoints(_rotationIndex);
+		MoveToNextPoint = false;
+		MoveToPrevPoint = false;
+		DisableCoolOff = true;
+	}
+
+	private void SetRotationPoints(int i)
+	{
+		_nextRotationPoint = _rotationPoint[i];
+		_prevRotationPoint = _rotationPoint[i-1];
 	}
 }
