b9cce800c10303e329dfdbff237642369da338cf
Requiem For The Hunter <brayden.mohring@gmail.com>
2024-02-28 16:45:02 +0000

    Begun creation of note system
    Current progress:
    - Note prefabs can be made and assigned to canvas
    - Interacting with something in the open which has the note interactable object allows you to open up the note prefab assigned to it
    - It totally does not crash. Nope. Not at all. (Do not test it idk if my script changes reverted)

--- StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
@@ -1,49 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class NoteMenu : MonoBehaviour
 {
 
     GameObject playerCharacter;
     GameObject playerHolder;
-    GameObject playerUI;
-    GameObject CurrentNote;
-
-    public bool IsNoteActive;
         
     public void Start()
     {
         playerCharacter = GameObject.FindWithTag("Player");
         playerHolder = playerCharacter.transform.parent.gameObject;
-        playerUI = GameObject.Find("PlayerUI");
-        
     }
 
-    public void Update()
+    public void ShowNote(GameObject Note)
     {
-
-        if (Input.GetKeyDown(KeyCode.E) && IsNoteActive)
-        {
-            Destroy(CurrentNote);
-            playerCharacter.SetActive(true);
-        }
-    }
-
-    public void SetNoteToGUI(GameObject Note)
-    {
+        playerCharacter.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
         Note = Instantiate(Note);
-        CurrentNote = Instantiate(Note);
-        if (CurrentNote != null)
-        {
-            CurrentNote.transform.SetParent(playerUI.transform);
-            CurrentNote.GetComponent<RectTransform>().localPosition = Vector3.zero;
-            Debug.Log("Patrolling the mojave desert almost makes you wish for a nuclear winter");
-            IsNoteActive = true;
-            playerCharacter.SetActive(false);
-            Debug.Log("War. War never changes.");
-        }
+        Note.transform.SetParent(playerHolder.transform.GetChild(1)); // gets the canvas, if the positioning of the canvas index changes then this will no longer work
+        Note.GetComponent<RectTransform>().localPosition = Vector3.zero;
     }
-
 }
