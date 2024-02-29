9e40a6c8504659af7746c53a8c47ced5cc49ab6d
DM <dommcneill@outlook.com>
2024-02-29 21:12:26 +0000

    Fixed the player
    Removed level 2

--- StudioUnityProject/Assets/Scripts/UI/MainMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/MainMenu.cs
@@ -7,87 +7,92 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("Volume Setting")]
-    [SerializeField] private TMP_Text volumeTextValue = null;
-    [SerializeField] private Slider volumeSlider = null;
+	[Header("Volume Setting")]
+	[SerializeField] private TMP_Text volumeTextValue = null;
+	[SerializeField] private Slider volumeSlider = null;
 
-    [Header("Gameplay Settings")]
-    [SerializeField] private TMP_Text ControllerSenTextValue = null;
-    [SerializeField] private Slider ControllerSenSlider = null;
-    public float mainControllerSen = 1f;
+	[Header("Gameplay Settings")]
+	[SerializeField] private TMP_Text ControllerSenTextValue = null;
+	[SerializeField] private Slider ControllerSenSlider = null;
+	public float mainControllerSen = 1f;
 
-    [Header("Toggle Settings")]
-    [SerializeField] private Toggle crouchToggle = null;
-    
+	[Header("Toggle Settings")]
+	[SerializeField] private Toggle crouchToggle = null;
 
-    [Header("Comfirmation")]
-    [SerializeField]  private GameObject comfirmationPrompt = null;
 
-    public void NewGameDialogYes()
-    {
-        //Loads Level 1 if player clicks yes
-        SceneManager.LoadScene(2);
-    }
+	[Header("Comfirmation")]
+	[SerializeField] private GameObject comfirmationPrompt = null;
 
-    //This is where the override code will be referenced/placed 
-    public void LoadGameDialogYes()
-    {
-        
-    }
+	void Start()
+	{
+		MouseLockManager.Instance.MouseVisable = true;
+	}
 
-    public void ExitButton()
-    {
-        //Allows the player to quit the application
-        Application.Quit();
-    }
+	public void NewGameDialogYes()
+	{
+		//Loads Level 1 if player clicks yes
+		LevelLoading.Instance.LoadScene(2);
+	}
 
-    public void SetVolume(float volume)
-    {
-        //Allows the player to change the volume
-        AudioListener.volume = volume;
-        volumeTextValue.text = volume.ToString("0.0");
-    }
- 
+	//This is where the override code will be referenced/placed 
+	public void LoadGameDialogYes()
+	{
 
-    public void VolumeSave()
-    {
-        //Saves the volume changes
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        StartCoroutine(ConfirmationBox());
-    }
+	}
 
-    public void SetControllerSen(float sensitivity)
-    {
-        //Allows the player to changed their mouse sensitivity
-        mainControllerSen = sensitivity;
-        ControllerSenTextValue.text = sensitivity.ToString("0.0");
-        
-    }
+	public void ExitButton()
+	{
+		//Allows the player to quit the application
+		Application.Quit();
+	}
 
-    public void GameplaySave()
-    {
-        //Allows the player to save their gameplay changes
-        if (crouchToggle.isOn)
-        {
-            //Allows crouch to become a toggle button instead of a crouch
-            PlayerPrefs.SetInt("masterCrouch", 1);
-        }
-        else
-        {
-            //Allows crouch to become a hold down button instead of a crouch
-            PlayerPrefs.SetInt("masterCrouch", 0);
-        }
+	public void SetVolume(float volume)
+	{
+		//Allows the player to change the volume
+		AudioListener.volume = volume;
+		volumeTextValue.text = volume.ToString("0.0");
+	}
 
-        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
-    }
 
-    public IEnumerator ConfirmationBox()
-    {
-        //Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
-        comfirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
-        comfirmationPrompt.SetActive(false);
-    }
+	public void VolumeSave()
+	{
+		//Saves the volume changes
+		PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+		StartCoroutine(ConfirmationBox());
+	}
 
-    
+	public void SetControllerSen(float sensitivity)
+	{
+		//Allows the player to changed their mouse sensitivity
+		mainControllerSen = sensitivity;
+		ControllerSenTextValue.text = sensitivity.ToString("0.0");
+
+	}
+
+	public void GameplaySave()
+	{
+		//Allows the player to save their gameplay changes
+		if (crouchToggle.isOn)
+		{
+			//Allows crouch to become a toggle button instead of a crouch
+			PlayerPrefs.SetInt("masterCrouch", 1);
+		}
+		else
+		{
+			//Allows crouch to become a hold down button instead of a crouch
+			PlayerPrefs.SetInt("masterCrouch", 0);
+		}
+
+		PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+	}
+
+	public IEnumerator ConfirmationBox()
+	{
+		//Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
+		comfirmationPrompt.SetActive(true);
+		yield return new WaitForSeconds(2);
+		comfirmationPrompt.SetActive(false);
+	}
+
+
 }
--- StudioUnityProject/Assets/Scripts/UI/PauseMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/PauseMenu.cs
@@ -3,130 +3,133 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using TMPro;
+using log4net.Core;
 
 public class PauseMenu : MonoBehaviour
 {
 
-    [Header("Volume Setting")]
-    [SerializeField] private TMP_Text volumeTextValue = null;
-    [SerializeField] private Slider volumeSlider = null;
+	[Header("Volume Setting")]
+	[SerializeField] private TMP_Text volumeTextValue = null;
+	[SerializeField] private Slider volumeSlider = null;
 
-    [Header("Gameplay Settings")]
-    [SerializeField] private TMP_Text ControllerSenTextValue = null;
-    [SerializeField] private Slider ControllerSenSlider = null;
-    public float mainControllerSen = 1f;
+	[Header("Gameplay Settings")]
+	[SerializeField] private TMP_Text ControllerSenTextValue = null;
+	[SerializeField] private Slider ControllerSenSlider = null;
+	public float mainControllerSen = 1f;
 
-    [Header("Toggle Settings")]
-    [SerializeField] private Toggle crouchToggle = null;
+	[Header("Toggle Settings")]
+	[SerializeField] private Toggle crouchToggle = null;
 
 
-    [Header("Comfirmation")]
-    [SerializeField] private GameObject comfirmationPrompt = null;
+	[Header("Comfirmation")]
+	[SerializeField] private GameObject comfirmationPrompt = null;
 
-    public static bool Paused = false;
-    public static bool Overiding = false;
-    public GameObject PauseMenuCanvas;
+	public static bool Paused = false;
+	public static bool Overiding = false;
+	public GameObject PauseMenuCanvas;
 
-    void Start()
-    {
-        Time.timeScale = 1f;
-    }
+	void Start()
+	{
+		Time.timeScale = 1f;
+		MouseLockManager.Instance.MouseVisable = false;
+	}
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape) && !Overiding)
-        {
-            if (Paused)
-            {
-                Play();
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            else
-            {
-                Stop();
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) && !Overiding)
+		{
+			if (Paused)
+			{
+				Play();
 
-    void Stop()
-    {
-        PauseMenuCanvas.SetActive(true);
-        Time.timeScale = 0f;
-        Paused = true;
+			}
+			else
+			{
+				Stop();
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+		}
+	}
 
-    }
+	void Stop()
+	{
+		PauseMenuCanvas.SetActive(true);
+		Time.timeScale = 0f;
+		Paused = true;
+		MouseLockManager.Instance.MouseVisable = true;
 
-    void Play()
-    {
-        PauseMenuCanvas.SetActive(false);
-        Time.timeScale = 1f;
-        Paused = false;
-    }
-    public void Resume()
-    {
-        PauseMenuCanvas.SetActive(false);
-        Time.timeScale = 1f;
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = false;
-    }
+	}
 
-    public void GameExitButton()
-    {
-        SceneManager.LoadScene(1);
-    }
-    public void ExitButton()
-    {
-        //Allows the player to quit the application
-        Application.Quit();
-    }
+	void Play()
+	{
+		MouseLockManager.Instance.MouseVisable = false;
+		PauseMenuCanvas.SetActive(false);
+		Time.timeScale = 1f;
+		Paused = false;
+	}
+	public void Resume()
+	{
+		PauseMenuCanvas.SetActive(false);
+		Time.timeScale = 1f;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = false;
+	}
 
-    public void SetVolume(float volume)
-    {
-        //Allows the player to change the volume
-        AudioListener.volume = volume;
-        volumeTextValue.text = volume.ToString("0.0");
-    }
+	public void GameExitButton()
+	{
+		LevelLoading.Instance.LoadScene(1);
+	}
+	public void ExitButton()
+	{
+		//Allows the player to quit the application
+		Application.Quit();
+	}
 
-    public void VolumeSave()
-    {
-        //Saves the volume changes
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        StartCoroutine(ConfirmationBox());
-    }
+	public void SetVolume(float volume)
+	{
+		//Allows the player to change the volume
+		AudioListener.volume = volume;
+		volumeTextValue.text = volume.ToString("0.0");
+	}
 
-    public void SetControllerSen(float sensitivity)
-    {
-        //Allows the player to changed their mouse sensitivity
-        mainControllerSen = sensitivity;
-        ControllerSenTextValue.text = sensitivity.ToString("0.0");
+	public void VolumeSave()
+	{
+		//Saves the volume changes
+		PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+		StartCoroutine(ConfirmationBox());
+	}
 
-    }
+	public void SetControllerSen(float sensitivity)
+	{
+		//Allows the player to changed their mouse sensitivity
+		mainControllerSen = sensitivity;
+		ControllerSenTextValue.text = sensitivity.ToString("0.0");
 
-    public void GameplaySave()
-    {
-        //Allows the player to save their gameplay changes
-        if (crouchToggle.isOn)
-        {
-            //Allows crouch to become a toggle button instead of a crouch
-            PlayerPrefs.SetInt("masterCrouch", 1);
-        }
-        else
-        {
-            //Allows crouch to become a hold down button instead of a crouch
-            PlayerPrefs.SetInt("masterCrouch", 0);
-        }
+	}
 
-        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
-    }
+	public void GameplaySave()
+	{
+		//Allows the player to save their gameplay changes
+		if (crouchToggle.isOn)
+		{
+			//Allows crouch to become a toggle button instead of a crouch
+			PlayerPrefs.SetInt("masterCrouch", 1);
+		}
+		else
+		{
+			//Allows crouch to become a hold down button instead of a crouch
+			PlayerPrefs.SetInt("masterCrouch", 0);
+		}
 
-    public IEnumerator ConfirmationBox()
-    {
-        //Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
-        comfirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
-        comfirmationPrompt.SetActive(false);
-    }
+		PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+	}
+
+	public IEnumerator ConfirmationBox()
+	{
+		//Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
+		comfirmationPrompt.SetActive(true);
+		yield return new WaitForSeconds(2);
+		comfirmationPrompt.SetActive(false);
+	}
 }
