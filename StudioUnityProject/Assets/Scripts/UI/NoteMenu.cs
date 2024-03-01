b8a9068eb8dfbdc2c9df5a5d41ea240f6d97e84a
Brayden <brayden.mohring@gmail.com>
2024-03-01 14:32:53 +0000

    Note System Changes/Progress
    - Fixed the actual system, can open up notes and close them
    - Player can no longer move when having a note opened
    
    Needs to be fixed/changed:
    - Need to add a overlay around the note to blur screen
    - Need to ensure that player is not disabled, disable other parts
    - Need to create wiki for it

--- StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class NoteMenu : MonoBehaviour
 {
 
     GameObject playerCharacter;
     GameObject playerHolder;
+    GameObject playerUI;
+    GameObject CurrentNote;
+
+    public bool IsNoteActive;
         
     public void Start()
     {
         playerCharacter = GameObject.FindWithTag("Player");
         playerHolder = playerCharacter.transform.parent.gameObject;
+        playerUI = GameObject.Find("PlayerUI");
+        
     }
 
-    public void ShowNote(GameObject Note)
+    public void Update()
     {
-        playerCharacter.transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
-        Note = Instantiate(Note);
-        Note.transform.SetParent(playerHolder.transform.GetChild(1)); // gets the canvas, if the positioning of the canvas index changes then this will no longer work
-        Note.GetComponent<RectTransform>().localPosition = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.E) && IsNoteActive)
+        {
+            Destroy(CurrentNote);
+            playerCharacter.SetActive(true);
+        }
     }
+
+    public void SetNoteToGUI(GameObject Note)
+    {
+        Note = Instantiate(Note);
+        CurrentNote = Instantiate(Note);
+        if (CurrentNote != null)
+        {
+            CurrentNote.transform.SetParent(playerUI.transform);
+            CurrentNote.GetComponent<RectTransform>().localPosition = Vector3.zero;
+            Debug.Log("Patrolling the mojave desert almost makes you wish for a nuclear winter");
+            IsNoteActive = true;
+            playerCharacter.SetActive(false);
+            Debug.Log("War. War never changes.");
+        }
+    }
+
 }
