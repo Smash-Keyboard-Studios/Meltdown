c66c2a87428025eb8f7562817f3774aef3cb2c38
Requiem For The Hunter <brayden.mohring@gmail.com>
2024-03-05 13:15:57 +0000

    Finished Note system
    Added:
    - Comments to the code
    - Background UI for the notes
    
    Fixed:
    - Note system not being correctly linked to the InputManager
    
    Removed:
    - Debug code lines

--- StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
@@ -7,10 +8,9 @@ using UnityEngine.PlayerLoop;
 public class NoteMenu : MonoBehaviour
 {
     GameObject playerCharacter;
-    GameObject playerHolder;
     GameObject playerUI;
     GameObject CurrentNote;
-    PauseMenu pausemenu;
+    GameObject NoteUIBackground;
 
     public bool IsNoteActive = false;
         
@@ -18,35 +18,37 @@ public class NoteMenu : MonoBehaviour
     {
 
         playerCharacter = GameObject.FindWithTag("Player");
-        playerHolder = playerCharacter.transform.parent.gameObject;
-        playerUI = GameObject.Find("PlayerUI");
+        playerUI = GameObject.Find("PlayerUI"); // Grabs both the player object and the player UI
+        NoteUIBackground = playerUI.transform.Find("NoteBackground").gameObject; // Grabs the NoteBackground to enable it
 
     }
 
     public void Update()
     {
         PauseMenu.Overiding = false;
-        PauseMenu.Paused = true;
-        if (Input.GetKeyDown(KeyCode.Escape) && IsNoteActive)
+        PauseMenu.Paused = true; 
+        if(Input.GetKeyDown(InputManager.GetKey(InputActions.KeyAction.UI)))
         {
-            Cursor.visible = false;
+
+            // Audio should be played here to indicate the closing of a note
             PauseMenu.Overiding = true;
-            PauseMenu.Paused = false;
-     
-            Destroy(CurrentNote);
+            PauseMenu.Paused = false; // This prevents the pause menu from opening up when trying to close a note
+
+            NoteUIBackground.SetActive(false); // Deactivates note background
+            Destroy(CurrentNote); // Destroys the note to prevent duplicates
             IsNoteActive = false;
-            playerCharacter.SetActive(true);
+            playerCharacter.SetActive(true); // Reactivates the player
         }
     }
 
-    public void SetNoteToGUI(GameObject Note)
+    public void SetNoteToGUI(GameObject Note) 
     {
-        CurrentNote = Instantiate(Note);
-            CurrentNote.transform.SetParent(playerUI.transform);
-            CurrentNote.GetComponent<RectTransform>().localPosition = Vector3.zero;
-            Debug.Log("Patrolling the mojave desert almost makes you wish for a nuclear winter");
-            playerCharacter.SetActive(false);
-            Debug.Log("War. War never changes.");
+        // Audio should be played here to indicate the opening of a note 
+        NoteUIBackground.SetActive(true); // Activate note background
+        CurrentNote = Instantiate(Note); // Retrieves the note prefab and instantiates it
+        CurrentNote.transform.SetParent(playerUI.transform);
+        CurrentNote.GetComponent<RectTransform>().localPosition = Vector3.zero; // Puts the note into the player UI and ensures its position is directly in the middle of the screen
+        playerCharacter.SetActive(false); // Prevents player from moving
         IsNoteActive = true;
     }
 
