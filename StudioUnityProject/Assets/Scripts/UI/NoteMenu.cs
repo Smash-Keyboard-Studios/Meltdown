a1d2698760a55f5c178ac65b61f09882f9fbe98f
Requiem For The Hunter <brayden.mohring@gmail.com>
2024-03-05 10:01:19 +0000

    Fixed Note Bug
    - Press escape to exit note.
    - One more feature needs to be implemented to finish it off.

--- StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/NoteMenu.cs
@@ -1,49 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 public class NoteMenu : MonoBehaviour
 {
-
     GameObject playerCharacter;
     GameObject playerHolder;
     GameObject playerUI;
     GameObject CurrentNote;
+    PauseMenu pausemenu;
 
-    public bool IsNoteActive;
+    public bool IsNoteActive = false;
         
     public void Start()
     {
+
         playerCharacter = GameObject.FindWithTag("Player");
         playerHolder = playerCharacter.transform.parent.gameObject;
         playerUI = GameObject.Find("PlayerUI");
-        
+
     }
 
     public void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.E) && IsNoteActive)
+        PauseMenu.Overiding = false;
+        PauseMenu.Paused = true;
+        if (Input.GetKeyDown(KeyCode.Escape) && IsNoteActive)
         {
+            Cursor.visible = false;
+            PauseMenu.Overiding = true;
+            PauseMenu.Paused = false;
+     
             Destroy(CurrentNote);
+            IsNoteActive = false;
             playerCharacter.SetActive(true);
         }
     }
 
     public void SetNoteToGUI(GameObject Note)
     {
-        Note = Instantiate(Note);
         CurrentNote = Instantiate(Note);
-        if (CurrentNote != null)
-        {
             CurrentNote.transform.SetParent(playerUI.transform);
             CurrentNote.GetComponent<RectTransform>().localPosition = Vector3.zero;
             Debug.Log("Patrolling the mojave desert almost makes you wish for a nuclear winter");
-            IsNoteActive = true;
             playerCharacter.SetActive(false);
             Debug.Log("War. War never changes.");
-        }
+        IsNoteActive = true;
     }
 
 }
