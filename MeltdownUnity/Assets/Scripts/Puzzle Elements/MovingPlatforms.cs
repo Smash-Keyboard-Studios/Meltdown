9cecf483419d1b70de5e51e5b58665efa2d4c7a0
Bio <aleksanderjkoloda@gmail.com>
2024-03-09 14:40:08 +0000

    Moving Platforms Updated
    Moving platform prefabs now have the option to be constantly moving and to be frozen by ice bullets, then unfrozen by fire bullets.

--- MeltdownUnity/Assets/Scripts/Puzzle Elements/MovingPlatforms.cs
+++ MeltdownUnity/Assets/Scripts/Puzzle Elements/MovingPlatforms.cs
@@ -7,6 +7,8 @@ public class MovingPlatforms : MonoBehaviour
 	public float PlatformSpeed = 1f;
 
 	public bool Activated = false;
+	public bool isConstant = false;
+	public bool isFrozen = false;
 
 	public Transform PlatformObject;
 	public Vector3 platformStartPos;
@@ -14,6 +16,10 @@ public class MovingPlatforms : MonoBehaviour
 
 	private float _timeCounter;
 
+	private void Start()
+	{
+		if (isConstant) { Activated = true; }
+	}
 	// Update is called once per frame
 	void Update()
 	{
@@ -25,13 +31,32 @@ public class MovingPlatforms : MonoBehaviour
 		// open
 		else if (_timeCounter > 0 && !Activated)
 		{
-			_timeCounter -= Time.deltaTime * PlatformSpeed;
+			_timeCounter -= Time.deltaTime * PlatformSpeed;		
 		}
-
+		if (_timeCounter <= 1 && isConstant) { ToggleDoor(); }
+		if (_timeCounter >= 0 && isConstant) { ToggleDoor(); }
 		// move platform.
 		PlatformObject.localPosition = Vector3.Lerp(platformStartPos, platformEndPos, _timeCounter);
 	}
-
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.GetComponent<Fire>() != null )
+		{
+			if (isFrozen)
+			{
+				PlatformSpeed = PlatformSpeed * 2.5f;
+				isFrozen = false; 
+			}
+		}
+		if (other.gameObject.GetComponent<Ice>() != null)
+		{
+			if (!isFrozen)
+			{
+				PlatformSpeed = PlatformSpeed * 0.4f;
+				isFrozen = true;
+			}
+		}
+	}
 	public void ToggleDoor()
 	{
 		Activated = !Activated;
