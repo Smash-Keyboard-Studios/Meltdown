d5e06c6f49a6bace0739e1b2fdb512e33773dc8b
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-03-11 16:30:51 +0000

    Audio Manager Development
    Just polishing scripts - needs further work and some sample audios so i can start adding audio to levels.

--- StudioUnityProject/Assets/Scripts/Managers/AudioManagers/MenuAudio.cs
+++ StudioUnityProject/Assets/Scripts/Managers/AudioManagers/MenuAudio.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuAudio : MonoBehaviour
 {
     public AudioSource MenuAudioSource;
     public AudioClipLibrary[] AudioLibraryArray;
