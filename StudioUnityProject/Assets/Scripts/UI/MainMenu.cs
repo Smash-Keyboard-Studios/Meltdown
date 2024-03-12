a2566c997baed9c6b032b42ed900711075e354cb
domibron <98326467+domibron@users.noreply.github.com>
2024-03-12 21:14:59 +0000

    Fixed merge left overs by removing
    Aaron, check pause menu and main menu

--- StudioUnityProject/Assets/Scripts/UI/MainMenu.cs
+++ StudioUnityProject/Assets/Scripts/UI/MainMenu.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6d4e228 (Added Scrolling Credits)
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -27,21 +23,14 @@ public class MainMenu : MonoBehaviour
 	[Header("Comfirmation")]
 	[SerializeField] private GameObject comfirmationPrompt = null;
 
-
-    public void NewGameDialogYes()
-	{
-		//Loads Level 1 if player clicks yes
-		SceneManager.LoadScene(2);
-	}
-
 	//This is where the override code will be referenced/placed 
 	public void LoadGameDialogYes()
 	{
-        SaveManager.current.ForceLoad();
-        if (SaveData.Current.CurrentLevel > 1)
-            LevelLoading.Instance.LoadScene(SaveData.Current.CurrentLevel);
-        else
-            LevelLoading.Instance.LoadScene(2);
+		SaveManager.current.ForceLoad();
+		if (SaveData.Current.CurrentLevel > 1)
+			LevelLoading.Instance.LoadScene(SaveData.Current.CurrentLevel);
+		else
+			LevelLoading.Instance.LoadScene(2);
 		SaveManager.current.ForceSave();
 	}
 
@@ -101,179 +90,3 @@ public class MainMenu : MonoBehaviour
 
 
 }
-=======
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System.Collections;
-using TMPro;
-
-
-public class MainMenu : MonoBehaviour
-{
-	[Header("Volume Setting")]
-	[SerializeField] private TMP_Text volumeTextValue = null;
-	[SerializeField] private Slider volumeSlider = null;
-
-	[Header("Gameplay Settings")]
-	[SerializeField] private TMP_Text ControllerSenTextValue = null;
-	[SerializeField] private Slider ControllerSenSlider = null;
-	public float mainControllerSen = 1f;
-
-	[Header("Toggle Settings")]
-	[SerializeField] private Toggle crouchToggle = null;
-
-
-	[Header("Comfirmation")]
-=======
->>>>>>> 6d4e228 (Added Scrolling Credits)
-	[SerializeField] private GameObject comfirmationPrompt = null;
-
-
-    public void NewGameDialogYes()
-	{
-		//Loads Level 1 if player clicks yes
-<<<<<<< HEAD
-		LevelLoading.Instance.LoadScene(2);
-    }
-
-	//This is where the override code will be referenced/placed 
-	public void LoadGameDialogYes()
-	{
-        SaveManager.current.ForceLoad();
-        if (SaveData.Current.CurrentLevel > 1)
-            LevelLoading.Instance.LoadScene(SaveData.Current.CurrentLevel);
-        else
-            LevelLoading.Instance.LoadScene(2);
-		SaveManager.current.ForceSave();
-	}
-
-	public void ExitButton()
-	{
-		//Allows the player to quit the application
-		Application.Quit();
-	}
-
-	public void SetVolume(float volume)
-	{
-		//Allows the player to change the volume
-		AudioListener.volume = volume;
-		volumeTextValue.text = volume.ToString("0.0");
-	}
-
-
-	public void VolumeSave()
-	{
-		//Saves the volume changes
-		PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-		StartCoroutine(ConfirmationBox());
-	}
-
-	public void SetControllerSen(float sensitivity)
-	{
-		//Allows the player to changed their mouse sensitivity
-		mainControllerSen = sensitivity;
-		ControllerSenTextValue.text = sensitivity.ToString("0.0");
-
-	}
-
-	public void GameplaySave()
-	{
-		//Allows the player to save their gameplay changes
-		if (crouchToggle.isOn)
-		{
-			//Allows crouch to become a toggle button instead of a crouch
-			PlayerPrefs.SetInt("masterCrouch", 1);
-		}
-		else
-		{
-			//Allows crouch to become a hold down button instead of a crouch
-			PlayerPrefs.SetInt("masterCrouch", 0);
-		}
-
-		PlayerPrefs.SetFloat("masterSen", mainControllerSen);
-	}
-
-	public IEnumerator ConfirmationBox()
-	{
-		//Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
-		comfirmationPrompt.SetActive(true);
-		yield return new WaitForSeconds(2);
-		comfirmationPrompt.SetActive(false);
-	}
-
-
-}
->>>>>>> 5b4ea08 (Credits done)
-=======
-		SceneManager.LoadScene(2);
-	}
-
-	//This is where the override code will be referenced/placed 
-	public void LoadGameDialogYes()
-	{
-        SaveManager.current.ForceLoad();
-        if (SaveData.Current.CurrentLevel > 1)
-            LevelLoading.Instance.LoadScene(SaveData.Current.CurrentLevel);
-        else
-            LevelLoading.Instance.LoadScene(2);
-		SaveManager.current.ForceSave();
-	}
-
-	public void ExitButton()
-	{
-		//Allows the player to quit the application
-		Application.Quit();
-	}
-
-	public void SetVolume(float volume)
-	{
-		//Allows the player to change the volume
-		AudioListener.volume = volume;
-		volumeTextValue.text = volume.ToString("0.0");
-	}
-
-
-	public void VolumeSave()
-	{
-		//Saves the volume changes
-		PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-		StartCoroutine(ConfirmationBox());
-	}
-
-	public void SetControllerSen(float sensitivity)
-	{
-		//Allows the player to changed their mouse sensitivity
-		mainControllerSen = sensitivity;
-		ControllerSenTextValue.text = sensitivity.ToString("0.0");
-
-	}
-
-	public void GameplaySave()
-	{
-		//Allows the player to save their gameplay changes
-		if (crouchToggle.isOn)
-		{
-			//Allows crouch to become a toggle button instead of a crouch
-			PlayerPrefs.SetInt("masterCrouch", 1);
-		}
-		else
-		{
-			//Allows crouch to become a hold down button instead of a crouch
-			PlayerPrefs.SetInt("masterCrouch", 0);
-		}
-
-		PlayerPrefs.SetFloat("masterSen", mainControllerSen);
-	}
-
-	public IEnumerator ConfirmationBox()
-	{
-		//Displays a confirmation window and starts a timer. Once that timer ends the window will vanish.
-		comfirmationPrompt.SetActive(true);
-		yield return new WaitForSeconds(2);
-		comfirmationPrompt.SetActive(false);
-	}
-
-
-}
->>>>>>> 6d4e228 (Added Scrolling Credits)
