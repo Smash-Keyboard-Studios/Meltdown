a9eae3e8d7a6eb24e730a539d61711d270af7bec
Adam <adamchagri77@gmail.com>
2024-03-15 14:17:34 +0000

    Lighting Improvements
    +Lighting to all of level 2
    +Lighting related puzzle in level 2 created
    +Lightmap Quality increased overall

--- MeltdownUnity/Assets/Scripts/Puzzle Elements/Buttons/PressureButton.cs
+++ MeltdownUnity/Assets/Scripts/Puzzle Elements/Buttons/PressureButton.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PressureButton : MonoBehaviour
 {
     [SerializeField] private bool buttonActive;
 
+
+    public UnityEvent OnButtonPress;
+
     private void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.GetComponent<Rigidbody>() != null || collision.gameObject.GetComponent<FirstPersonController>() != null)
+        print("i want to be free");
+        if(collision.gameObject.GetComponent<CharacterController>() != null || collision.gameObject.GetComponent<Rigidbody>() || collision.gameObject.GetComponent<FirstPersonController>() != null)
         {
-            buttonActive = true;
-        }
-    }
-
-    private void OnTriggerStay(Collider collision)
-    {
-        if (collision.gameObject.GetComponent<Rigidbody>() != null || collision.gameObject.GetComponent<FirstPersonController>() != null)
-        {
-            buttonActive = true;
+            OnButtonPress.Invoke();
         }
     }
 
     private void OnTriggerExit(Collider collision)
     {
         buttonActive = false;
+        OnButtonPress.Invoke();
     }
 }
