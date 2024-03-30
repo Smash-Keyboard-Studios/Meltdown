ce180f17b824d2a3e8af47c23bbd5185ccb5ac8e
DM <dommcneill@outlook.com>
2024-03-30 02:04:05 +0000

    Comments added to Player move and look as well as projectile

--- MeltdownUnity/Assets/Scripts/Player/Player movement/ItemSway.cs
+++ MeltdownUnity/Assets/Scripts/Player/Player movement/ItemSway.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class ItemSway : MonoBehaviour
 {
-	[SerializeField] private float _smooth;
-	[SerializeField] private float _swayMultiplier;
+	[SerializeField, Tooltip("How fast to return the object bac; to the normal rotation")]
+	private float _smooth;
 
-	private void Start()
-	{
-
-	}
+	[SerializeField, Tooltip("How noticable the sway is")]
+	private float _swayMultiplier;
 
 	private void Update()
 	{
+		// we want to get the direction the player is moving their mouse.
 		float mouseX = Input.GetAxisRaw("Mouse X") * _swayMultiplier;
 		float mouseY = Input.GetAxisRaw("Mouse Y") * _swayMultiplier;
 
+		// invert the rotation as a counter rotation.
 		Quaternion rotationX = Quaternion.AngleAxis(mouseY, Vector3.right);
 		Quaternion rotationY = Quaternion.AngleAxis(-mouseX, Vector3.up);
 
+		// Get the target rotation from both counter rotation.
 		Quaternion targetRotation = rotationX * rotationY;
 
+		// Use lerp to control the speed at which the rotation is returned to normal.
 		transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, _smooth * Time.deltaTime);
 	}
 }
--- MeltdownUnity/Assets/Scripts/Player/Player movement/MouseLookController.cs
+++ MeltdownUnity/Assets/Scripts/Player/Player movement/MouseLookController.cs
@@ -4,91 +4,116 @@ using UnityEngine;
 
 public class MouseLookController : MonoBehaviour
 {
-	[HideInInspector] // no need to touch this
+	[Header("Look locking")]
+
+	[Tooltip("Stops the player from being able to look around")]
 	public bool Locked = false;
 
+	[Header("Variables that need to be set")]
 
+	[Tooltip("We need this to rotate up and down")]
 	public Transform CameraHolder;
 
-	public float Sensitivity = 1f;
 
-
-	private Transform _playerBody;
-
-	private float _yRotation;
-
-
-	// head bobing
 	[Header("Head Bob")]
-	public Transform HeadBob;
-	public float headBobHorizontalAmplitude;
-	public float headBobVerticalAmplitude;
-	public float maxBobFreq = 4.5f;
-	[Range(0, 1)] public float headBobSmoothing;
 
-	private float xRotation;
-	private float yRotation;
-	private float headBobFrequency;
-	private float walkingTime;
+	[Tooltip("The object we move for bobbing")]
+	public Transform HeadBobObject;
+
+	[Tooltip("How much we move left and right")]
+	public float HeadBobHorizontalAmplitude;
+
+	[Tooltip("How much we move up and down")]
+	public float HeadBobVerticalAmplitude;
+
+	[Tooltip("How much we bob")]
+	public float MaxBobFreq = 4.5f;
+
+	[Range(0, 1), Tooltip("Lerp smoothing")] public float HeadBobSmoothing;
+
+
+	// storing stuff
+	private float _headBobFrequency;
+	private float _walkingTime;
 	private Vector3 targetCameraPosition;
 
 	private PlayerMovementController movementController;
 	private CharacterController characterController;
 
+	private float _sensitivity = 1f;
+	private Transform _playerBody;
+	private float _yRotation;
 
 
-	// Start is called before the first frame update
 	void Start()
 	{
 		// what is this? what is this bro? :face_vomiting:
+		// yeah, like, you can just use transform.
 		_playerBody = GetComponent<Transform>();
 
+		// get other components.
 		movementController = GetComponent<PlayerMovementController>();
 		characterController = GetComponent<CharacterController>();
 
+		// we set the sensitivity.
+		if (SaveManager.current != null)
+		{
+			SaveManager.current.ForceLoad();
+			_sensitivity = SaveData.Current.Sensitivity;
+		}
+
+		// stops frustrating mistakse.
 		Locked = false;
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		if (Locked) return;
+		HandleLook();
 
-		Vector2 lookDirection = new();
-
-		lookDirection.x = Input.GetAxisRaw("Mouse X");
-		lookDirection.y = Input.GetAxisRaw("Mouse Y");
-
-		_yRotation -= lookDirection.y;
-		_yRotation = Mathf.Clamp(_yRotation, -80f, 90f);
-
-		// could put sensitivity here, like ved2D * sens
-
-		_playerBody.Rotate(Vector3.up * lookDirection.x * Sensitivity);
-
-
-		CameraHolder.localRotation = Quaternion.Euler(_yRotation * Sensitivity, 0, 0);
-
+		// we start head bob.
 		MainHeadBobbing();
 	}
 
+	private void HandleLook()
+	{
+		// if we are locked than dont do anything.
+		if (Locked) return;
 
+		// create a new vector 2.
+		Vector2 lookDirection = new();
 
+		// we store the mouse movements.
+		lookDirection.x = Input.GetAxisRaw("Mouse X");
+		lookDirection.y = Input.GetAxisRaw("Mouse Y");
 
+		// we invert because screens start are top left, so positive means down.
+		_yRotation -= lookDirection.y;
+
+		// we clamp the rotation. obvious why we do this.
+		_yRotation = Mathf.Clamp(_yRotation, -80f, 90f);
+
+		// we rotate the body.
+		_playerBody.Rotate(Vector3.up * lookDirection.x * _sensitivity);
+
+		// we rotate the cam holder. (child of the player)
+		CameraHolder.localRotation = Quaternion.Euler(_yRotation * _sensitivity, 0, 0);
+	}
+
+	// I refuse to explain the math behind this.
 	private void MainHeadBobbing()
 	{
+		// we use the character controller because if we are running into a wall, we wont bob.
 		Vector3 MovementVel = new Vector3(characterController.velocity.x, 0, characterController.velocity.z);
-		if (MovementVel.magnitude > 0 && movementController.isGrounded) walkingTime += Time.deltaTime;
-		else walkingTime = 0f;
+		if (MovementVel.magnitude > 0 && movementController.isGrounded) _walkingTime += Time.deltaTime;
+		else _walkingTime = 0f;
 
-		headBobFrequency = (movementController.speed > maxBobFreq) ? 1f * movementController.speed : maxBobFreq;
+		_headBobFrequency = (movementController.speed > MaxBobFreq) ? 1f * movementController.speed : MaxBobFreq;
 
+		targetCameraPosition = CameraHolder.position + CalculateHeadBobOffset(_walkingTime);
 
-		targetCameraPosition = CameraHolder.position + CalculateHeadBobOffset(walkingTime);
+		HeadBobObject.position = Vector3.Lerp(HeadBobObject.transform.position, targetCameraPosition, HeadBobSmoothing);
 
-		HeadBob.position = Vector3.Lerp(HeadBob.transform.position, targetCameraPosition, headBobSmoothing);
-
-		if ((HeadBob.position - targetCameraPosition).magnitude <= 0.001) HeadBob.position = targetCameraPosition;
+		if ((HeadBobObject.position - targetCameraPosition).magnitude <= 0.001) HeadBobObject.position = targetCameraPosition;
 	}
 
 	private Vector3 CalculateHeadBobOffset(float t)
@@ -99,8 +124,8 @@ public class MouseLookController : MonoBehaviour
 
 		if (t > 0)
 		{
-			horOffset = Mathf.Cos(t * headBobFrequency) * headBobHorizontalAmplitude * characterController.height / 2;
-			vertOffset = Mathf.Sin(t * headBobFrequency * 2f) * headBobVerticalAmplitude * characterController.height / 2;
+			horOffset = Mathf.Cos(t * _headBobFrequency) * HeadBobHorizontalAmplitude * characterController.height / 2;
+			vertOffset = Mathf.Sin(t * _headBobFrequency * 2f) * HeadBobVerticalAmplitude * characterController.height / 2;
 
 			Offset = CameraHolder.right * horOffset + CameraHolder.up * vertOffset;
 		}
